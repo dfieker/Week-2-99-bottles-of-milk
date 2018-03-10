c92f08d6fa8f3cfbce849874a9ed5c364821bb47
dfieker <danielmfieker@gmail.com>
2018-03-10 09:45:31 -0600

    Working 99 bottles of milk program

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var milk = 99;
+
+// repeat the section 99 times
+while(milk > 0) {
+if(milk === 1) {
+console.log(milk + " bottles of milk on the wall");
+console.log(milk + " bottles of milk");
+console.log("you take one down, you pass it around." );
+	milk--;
+console.log("No more bottles of milk on the wall...");
+} else {
+	console.log(milk + " bottles of milk on the wall");
+console.log(milk + " bottles of milk");
+console.log("you take one down, you pass it around." );
+	milk--;
+console.log(milk + " bottles of milk on the wall");
+console.log("");
+// milk = milk - 1;
+}
+}
