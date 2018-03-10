9f7dff671062ee29d6baa25d8c1ef4a6fc00e057
dfieker <danielmfieker@gmail.com>
2018-03-10 10:29:42 -0600

    Moving on to functions

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 var milk = 99;
 
+function printVerse(num){
+
+	var name = "bottles";
+	if(num === 1) { name = "bottle"; }
+
+	console.log(num + " " + name + " of milk on the wall");
+	console.log(num + " " + name + " of milk");
+	console.log("you take one down, you pass it around." );
+	num--;
+	console.log(num + " bottles of milk on the wall\n");
+	return num;	
+}
+
 // repeat the section 99 times
 while(milk > 0) {
-if(milk === 1) {
-console.log(milk + " bottles of milk on the wall");
-console.log(milk + " bottles of milk");
-console.log("you take one down, you pass it around." );
-	milk--;
-console.log("No more bottles of milk on the wall...");
-} else {
-	console.log(milk + " bottles of milk on the wall");
-console.log(milk + " bottles of milk");
-console.log("you take one down, you pass it around." );
-	milk--;
-console.log(milk + " bottles of milk on the wall");
-console.log("");
-// milk = milk - 1;
-}
-}
+	//  console.log("No more bottles of milk on the wall...");
+	milk = printVerse(milk);
+	}
